ac816d125412e2b102299cf76b384b2272ce17fa
Kelly Lee <amorfati38@gmail.com>
2022-03-16 00:42:22 -0600

    fix login and out err

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -26,6 +27,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
 // turn on routes
