f425167f618d02038da36635a8507a25e6549d50
Kelly Lee <amorfati38@gmail.com>
2022-03-16 22:33:45 -0600

    partials and add helpers

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,6 +21,8 @@ const sess = {
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
